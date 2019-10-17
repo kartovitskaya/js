01e745bf28e563507bd1d6464a5d30fc5da25409
kxenia <kkartovitskaya@gmail.com>
2019-10-17 12:29:40 +0200

    Home work done

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,6 +1,13 @@
 // myVar - имя переменной(CamelCase). 10 - ее значение.
 var myVar = 30;
 
+//Задание
+var money = 1;
+var income = 2;
+var addExpenses = 3;
+var deposit = 4;
+var mission = 5;
+var period = 6;
 
-
-var myVar = 20;
+alert(1);
+console.log(2);
