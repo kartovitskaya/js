15b76a68f63d05a3529ce2c40c9ffedde19904d0
kxenia <kkartovitskaya@gmail.com>
2019-10-19 12:42:09 +0200

    var changed to let

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,13 +1,13 @@
 // myVar - имя переменной(CamelCase). 10 - ее значение.
-var myVar = 30;
+let myVar = 30;
 
 //Задание
-var money = 1;
-var income = 2;
-var addExpenses = 3;
-var deposit = 4;
-var mission = 5;
-var period = 6;
+let money = 1;
+let income = 2;
+let addExpenses = 3;
+let deposit = 4;
+let mission = 5;
+let period = 6;
 
 alert(1);
 console.log(2);
