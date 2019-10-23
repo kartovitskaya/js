c0ea5f9c993bbd9f4a7fecdaca83eb52f8eb7fae
kartovitskaya <33638024+kartovitskaya@users.noreply.github.com>
2019-10-23 22:22:09 +0200

    Add income

--- lesson/script.js
+++ lesson/script.js
@@ -1,4 +1,41 @@
 //Задание
+
+let money = 300;
+    income = 'taxi';
+    addExpenses = 'Food, Water, Study';
+    deposit = false;
+    mission = 100;
+    period = 5;
+    budgetDay= money/30;
+    budgetDayRest= money%30;
+
+console.log(typeof money, typeof income, typeof deposit);
+
+console.log(income.length);
+
+console.log('Период месяцев:', period);
+
+console.log('Цель заработать', mission, 'рублей/долларов/гривен/юани');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
+console.log(budgetDayRest);
+
+
+//Задание 2
+ 
+let num = 266219;
+    numResult = persistence(num)**3;
+
+function persistence(num) {
+    return [...num.toString()].reduce((p, v) => p * v);
+}
+console.log(persistence(num));
+
+console.log(numResult.toString.substring(0, 2));
+
+//Задание
 let money = prompt('Ваш месячный доход?');
 
 addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
