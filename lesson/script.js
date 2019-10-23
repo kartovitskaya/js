947541afd21dc7a203d46933ad8e4095fd5fe9ed
kxenia <kkartovitskaya@gmail.com>
2019-10-23 13:49:03 +0200

    Start Extra lesson03

--- lesson/script.js
+++ lesson/script.js
@@ -1,44 +1,8 @@
 //Задание
-    let money = prompt('Ваш месячный доход?');
-
-        addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-        console.log(addExpenses.split(', '));
-
-        deposit = confirm('Есть ли у вас депозит в банке?');
-
-        console.log(typeof money, typeof income, typeof deposit);
-
-        yourMonthlyExpensesFirst = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-        howMuchFirst = prompt('Во сколько это обойдется?');
-
-        yourMonthlyExpensesSecond = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-        howMuchSecond = prompt('Во сколько это обойдется?');
-
-        budgetMonth = money - howMuchFirst;
-        console.log(budgetMonth);
-
-    let mission = 100;
-        missionMonth = mission/budgetMonth;
-        console.log(Math.ceil(missionMonth));
-
-    let budgetDay = budgetMonth/30;
-        console.log(Math.floor(budgetDay));
-    
-    switch(true){
-            case (budgetDay > 800):
-                console.log('Высокий уровень дохода');
-                break;
-            case (300 <= budgetDay && budgetDay < 800):
-                console.log('Средний уровень дохода');
-                break;
-            case (0 <= budgetDay && budgetDay < 300):
-                console.log('Низкий уровень дохода');
-                break;
-            case (budgetDay < 0):
-                console.log('Что то пошло не так');
-                break;
-            // default: 
-            //     console.log('не верно');
-            }
-
-
+let enDays = Monday, Thuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+    ruDays = Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье;
+if (lang =en) {
+    console.log(enDays);
+} else {
+    console.log(ruDays);
+}
