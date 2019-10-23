89bc4528ac9006c5085d1330efb797fd83d2e548
kxenia <kkartovitskaya@gmail.com>
2019-10-23 14:52:39 +0200

    Extra homework lesson 03 done

--- lesson/script.js
+++ lesson/script.js
@@ -1,8 +1,30 @@
-//Задание
-let enDays = Monday, Thuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
-    ruDays = Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье;
-if (lang =en) {
+'use strict';
+//Задание 1
+let enDays = 'Monday, Thuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
+    ruDays = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
+if (lang = "en") {
     console.log(enDays);
 } else {
     console.log(ruDays);
 }
+
+switch(true) {
+        case (lang = "en"):
+            console.log(enDays);
+            break;
+        case (lang ="ru") :
+            console.log(ruDays);
+            break;
+}
+
+let result = lang = "en " ? enDays : ruDays;
+    console.log('result:', result);
+
+//Задание2
+
+let namePerson = prompt('Введите имя');
+        role = 
+        (namePerson === 'Артём') ? 'директор':
+        (namePerson === 'Максим') ? 'преподаватель': 'студент';
+    
+    console.log(role);
