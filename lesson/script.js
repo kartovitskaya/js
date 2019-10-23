1fb193a10b0a2c98f75dbe2431b8de60ba029492
kartovitskaya <33638024+kartovitskaya@users.noreply.github.com>
2019-10-23 14:12:31 +0200

    Edit homework

--- lesson/script.js
+++ lesson/script.js
@@ -1,44 +1,43 @@
 //Задание
-    let money = prompt('Ваш месячный доход?');
+let money = prompt('Ваш месячный доход?');
 
-        addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-        console.log(addExpenses.split(', '));
+addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(addExpenses.split(', '));
 
-        deposit = confirm('Есть ли у вас депозит в банке?');
+deposit = confirm('Есть ли у вас депозит в банке?');
 
-        console.log(typeof money, typeof income, typeof deposit);
+console.log('money:', typeof money, 'income:', typeof income, 'deposit:', typeof deposit);
 
-        yourMonthlyExpensesFirst = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-        howMuchFirst = prompt('Во сколько это обойдется?');
+yourMonthlyExpensesFirst = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+howMuchFirst = +prompt('Во сколько это обойдется?');
 
-        yourMonthlyExpensesSecond = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-        howMuchSecond = prompt('Во сколько это обойдется?');
+yourMonthlyExpensesSecond = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+howMuchSecond = +prompt('Во сколько это обойдется?');
 
-        budgetMonth = money - howMuchFirst;
-        console.log(budgetMonth);
+howMuch = howMuchFirst + howMuchSecond;
+budgetMonth = money - howMuch;
+console.log(budgetMonth);
 
-    let mission = 100;
-        missionMonth = mission/budgetMonth;
-        console.log(Math.ceil(missionMonth));
+let mission = 100;
+missionMonth = mission/budgetMonth;
+console.log(Math.ceil(missionMonth));
 
-    let budgetDay = budgetMonth/30;
-        console.log(Math.floor(budgetDay));
-    
-    switch(true){
-            case (budgetDay > 800):
-                console.log('Высокий уровень дохода');
-                break;
-            case (300 <= budgetDay && budgetDay < 800):
-                console.log('Средний уровень дохода');
-                break;
-            case (0 <= budgetDay && budgetDay < 300):
-                console.log('Низкий уровень дохода');
-                break;
-            case (budgetDay < 0):
-                console.log('Что то пошло не так');
-                break;
-            // default: 
-            //     console.log('не верно');
-            }
+let budgetDay = budgetMonth/30;
+console.log(Math.floor(budgetDay));
 
-
+switch(true){
+    case (budgetDay > 800):
+        console.log('Высокий уровень дохода');
+        break;
+    case (300 <= budgetDay && budgetDay < 800):
+        console.log('Средний уровень дохода');
+        break;
+    case (0 <= budgetDay && budgetDay < 300):
+        console.log('Низкий уровень дохода');
+        break;
+    case (budgetDay < 0):
+        console.log('Что то пошло не так');
+        break;
+    default: 
+        console.log('не верно');
+    }
