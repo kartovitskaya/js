2fd5c8aec2d16fe080c9d0d5cf2caebba8997c3e
kxenia <kkartovitskaya@gmail.com>
2019-10-31 10:26:14 +0100

    Practice done

--- lesson/script.js
+++ lesson/script.js
@@ -24,11 +24,24 @@ let appData = {
     budgetDay: 0, 
     budgetMonth: 0, 
     expensesMonth: 0,
-    
+    dataTypeNumber:function (n){
+		while (isNaN(n) || n === '' || n === null){
+			n = prompt('Вы ввели не верное значение. Повторите попытку.');
+		}
+		return Number(n);
+    },
+    dataTypeString:function (s){
+		while (!isNaN(s) || s == '' || s === null){
+			s = prompt('Вы ввели не верное значение. Повторите попытку.');
+		}
+		return String(s);
+	},
     asking: function(){
         if(confirm('Есть ли у вас дополнительный заработок?')){
             let itemIncome = prompt('Какой у вас дополнительный заработок?', 'Такси');
+            appData.itemIncome = appData.dataTypeString(itemIncome);
             let cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
+            appData.cashIncome = appData.dataTypeNumber(cashIncome);
             appData.income[itemIncome] = cashIncome;
         }
         let addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
@@ -39,12 +52,14 @@ let appData = {
 		let answerSecond;
 		for (let i = 0; i < 2; i++){
 			if (i == 0){
-				let questionFirst = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Комунальные платежи');
+                let questionFirst = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Комунальные платежи');
+                appData.questionFirst = appData.dataTypeString(questionFirst);
 				answerFirst = prompt('Во сколько это обойдется?', 2500);
 				appData.expenses[questionFirst] = +answerFirst;
 			}
 			if (i == 1){
-				let questionSecond = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Питание');
+                let questionSecond = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Питание');
+                appData.questionSecond = appData.dataTypeString(questionSecond);
 				answerSecond = prompt('Во сколько это обойдется?', 2500);
 				appData.expenses[questionSecond] = +answerSecond;
 			}
@@ -77,10 +92,13 @@ let appData = {
 		}  else if (appData.budgetDay < 0) {
 			return 'Что-то пошло не так';
 		}
-    },getInfoDeposit: function(){
+    },
+    getInfoDeposit: function(){
         if(appData.deposit){
             appData.percentDeposit = prompt('Какой годовой процент?', '10');
-            appData.moneyDeposit = prompt('какая сумма заложена?', 10000);
+            appData.dataTypeNumber(appData.percentDeposit);
+            appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+            appData.dataTypeNumber(appData.moneyDeposit);
         }
     },
     calcSavedMoney: function(){
@@ -94,8 +112,17 @@ let appData = {
     console.log('Расходы за месяц: ' + appData.expensesMonth);
     appData.getTargetMonth();
     console.log (appData.getStatusIncome());
+    appData.getInfoDeposit();
+    console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney());
+    
 
     console.log('Наша программа включает в себя данные:')
         for (let key in appData){
 	console.log('Ключ: ' + key, 'Значение: ' + appData[key]);
 }
+
+let textAddExpenses = function(){
+    appData.addExpenses = appData.addExpenses.map((item) => item[0].toUpperCase() + item.slice(1).toLowerCase());
+    textAddExpenses = appData.addExpenses[i] + ', ';
+}
+console.log(textAddExpenses());
