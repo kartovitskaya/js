0732cb6c300219424ab73231ed46b04f679255d6
kartovitskaya <33638024+kartovitskaya@users.noreply.github.com>
2019-11-05 21:54:10 +0100

    Edit bookSecond

--- lesson07/you-dont-know-js/script/script.js
+++ lesson07/you-dont-know-js/script/script.js
@@ -8,34 +8,28 @@ books[0].insertBefore(book[1], book[0]);
 let body = document.querySelector('body');
 body.setAttribute('style', 'background-image: url(image/you-dont-know-js.jpg)');
 
-let nameBook3 = book[4].querySelector('a');
-nameBook3.textContent = 'Книга 3. this и Прототипы Объектов';
-
 let banner = document.querySelector('.adv');
 banner.remove();
 
-let ulBookSecond = book[0].querySelector('ul'),
-    liBookSecond = book[0].querySelectorAll('li');
+let nameBookThird = book[4].querySelector('a');
+nameBookThird.textContent = 'Книга 3. this и Прототипы Объектов';
 
+let ulBookSecond = book[0].querySelector('ul');
+let liBookSecond = book[0].querySelectorAll('li');
 ulBookSecond.insertBefore(liBookSecond[6], liBookSecond[4]);
 ulBookSecond.insertBefore(liBookSecond[8], liBookSecond[4]);
 ulBookSecond.insertBefore(liBookSecond[2], liBookSecond[10]);
 
-let ulBookFifth = book[5].querySelector('ul'),
-    liBookFifth = book[5].querySelectorAll('li');
+let ulBookFifth = book[5].querySelector('ul');
+let liBookFifth = book[5].querySelectorAll('li');
 
-    ulBookFifth.insertBefore(liBookFifth[2], liBookFifth[6]);
-    ulBookFifth.insertBefore(liBookFifth[9], liBookFifth[3]);
-    ulBookFifth.insertBefore(liBookFifth[5], liBookFifth[8]);
-    ulBookFifth.removeChild(liBookFifth[12]);
-    ulBookFifth.insertBefore(liBookFifth[10], liBookFifth[8]);
+let addABookFifth = ulBookFifth.replaceChild(liBookFifth[2], liBookFifth[5]);
+ulBookFifth.insertBefore(addABookFifth, liBookFifth[8]);
+ulBookFifth.insertBefore(liBookFifth[9], liBookFifth[3]);
 
+let titleEight = document.createElement('li');
+titleEight.textContent = 'Глава 8: За пределами ES6';
 
-let newItem = document.createElement('li');
-newItem.Content = "Глава 8: За пределами ES6";
-ulBookFifth.appendChild(newItem);
-
-
-let liBookSixth = ulBookFifth.querySelectorAll('li');
-ulBookFifth.insertBefore(liBookSixth[10], liBookSixth[9]);
-
+let ulBookSixth = book[2].querySelector('ul');
+let liBookSixth = book[2].querySelectorAll('li');
+ulBookSixth.insertBefore(titleEight, liBookSixth[9]);
